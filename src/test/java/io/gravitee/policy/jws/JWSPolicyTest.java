12641f9062fa80b5130cebabefce4c23f1ecd820
Yann Tavernier <yann.tavernier@graviteesource.com>
2022-03-02 09:35:40 +0100

    fix(test): change serialNumber to get an existing revoked certificate

--- src/test/java/io/gravitee/policy/jws/JWSPolicyTest.java
+++ src/test/java/io/gravitee/policy/jws/JWSPolicyTest.java
@@ -245,7 +245,8 @@ public class JWSPolicyTest {
         X509Certificate cert = (X509Certificate) certificateFactory.generateCertificate(
             new ByteArrayInputStream(fileContent.getBytes(StandardCharsets.UTF_8))
         );
-        jwsPolicy.validateCRLSFromCertificate(cert, new BigInteger("17521746654160821224073118409816269840"));
+        // WARNING: this test is based on real certificate from Wikipedia. Depending on the state of the expired certificates entries, it could be broken.
+        jwsPolicy.validateCRLSFromCertificate(cert, new BigInteger("18135611436097276018781887707242686781"));
     }
 
     private void shouldTransformInput_validX5CHeader_withPemFile(String pemFile) throws Exception {
